5969ad6ffd174cd98bf0ce92c7cee66f3587a005
Andrew Minarick <andrew.minarick@gmail.com>
2018-05-15 17:29:53 -0500

    updated variable usage and check within templates

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
@@ -31,16 +32,27 @@ class NewVisitorTest(unittest.TestCase):
 
 		#updates and lists her item in a list
 		inputbox.send_keys(Keys.ENTER)
+		import time
+		time.sleep(10)
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy Peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
-		)
+
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
 
 		#she is invited to add another, page repdates
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
-
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy pacock feathers', [row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly' ,
+			[row.text for row in rows]
+		)
+		
 		#user wants to remember the list, then sees site has 
 		#generated unique url
 
