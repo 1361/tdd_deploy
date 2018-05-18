0d116555272fdd99a4895d4784aac08f6ab66b0a
Andrew Minarick <andrew.minarick@gmail.com>
2018-05-18 17:36:45 -0500

    worked with templates and new urls pg 96

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -44,7 +44,7 @@ class NewVisitorTest(LiveServerTestCase):
         import time
         time.sleep(5)
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
+        self.assertRegex(edith_list_url, '/lists.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 #
         # she is invited to add another, page repdates
--- lists/views.py
+++ lists/views.py
@@ -6,15 +6,14 @@ from lists.models import Item
 # Create your views here.
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-        #    else:
-        #        new_item_text = ''
-#    items = Item.objects.all()
+
     return render(request, 'home.html')
 #                 {'new_item_text': new_item_text,}
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
