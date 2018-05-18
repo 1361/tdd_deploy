f661d1743c3c3894ff064d07f0aa60fe497a1c75
Andrew Minarick <andrew.minarick@gmail.com>
2018-05-18 15:59:29 -0500

    page 86 TDD

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,20 +40,15 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         # updates and lists her item in a list
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+
         import time
         time.sleep(5)
 
-#        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        #		table = self.browser.find_element_by_id('id_list_table')
-        #		rows = table.find_elements_by_tag_name('tr')
-        #		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        #		self.assertTrue(
-        #			any(row.text == '1: Buy peacock feathers' for row in rows),
-        #			"new to-do item did not appear in table -- it was: \n%s" % (
-        #				table.text,
-        #			)
-        #		)
-
+#
         # she is invited to add another, page repdates
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
@@ -62,19 +57,38 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        #		table = self.browser.find_element_by_id('id_list_table')
-        #		rows = table.find_elements_by_tag_name('tr')
-
-        #		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        #		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # user wants to remember the list, then sees site has
+
         # generated unique url
 
         # user visits list
+        # need new browser session to ensure no info is left over
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        #user visits home page, no sign of other users' lists
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        #user starts own new list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        #new user gets own unique url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #still no trace of previous user's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        #both are satisfied
 
         self.fail('Finish the test!')
 
-
-#if __name__ == '__main__':
- #   unittest.main(warnings='ignore')
