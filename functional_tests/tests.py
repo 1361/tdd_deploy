52fe16d9ed2a65666eccc9933073b92014a9675c
Andrew Minarick <andrew.minarick@gmail.com>
2018-06-01 15:54:45 -0500

    pre push

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import sys
 import unittest
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys
@@ -7,6 +8,21 @@ browser = webdriver.Firefox()
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                    cls.server_url= 'http://' + arg.split('=')[1]
+                    return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,31 +37,31 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
 
-    def test_layout_and_styling(self):
-        #Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        #she notices the input box is centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width']/2,
-            510,
-            delta=5
-        )
-        #starts a new list and it is centered too
-        inputbox.send_keys('testing\n')
-        inputbox =  self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width']/2,
-            510,
-            delta=5
-        )
+    # def test_layout_and_styling(self):
+    #     #Edith goes to the home page
+    #     self.browser.get(self.server_url)
+    #     self.browser.set_window_size(1024, 768)
+    #
+    #     #she notices the input box is centered
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     self.assertAlmostEqual(
+    #         inputbox.location['x'] + inputbox.size['width']/2,
+    #         510,
+    #         delta=5
+    #     )
+    #     #starts a new list and it is centered too
+    #     inputbox.send_keys('testing\n')
+    #     inputbox =  self.browser.find_element_by_id('id_new_item')
+    #     self.assertAlmostEqual(
+    #         inputbox.location['x'] + inputbox.size['width']/2,
+    #         510,
+    #         delta=5
+    #     )
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # user goes to website
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # user sees title of 'to-do' list
         self.assertIn('To-Do', self.browser.title)
@@ -90,7 +106,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         #user visits home page, no sign of other users' lists
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
