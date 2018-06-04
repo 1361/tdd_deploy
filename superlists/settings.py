95584787af485abc694cdb3bd175c5f70540d711
1361 <andrew.minarick@gmail.com>
2018-06-04 14:18:59 -0500

    Update settings.py
    removed key

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pyx4)ui2zg$s)f!*tms=o*hzb)^%h7mwy14_yp*xsh!98h1$9!'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
